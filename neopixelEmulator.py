ca90fc553453175099bcc0badfff3d61e478a937
Footleg <drfootleg@gmail.com>
2017-12-27 12:21:08 +0000

    Implemented show method, and wrote example. Demo now runs.

--- neopixelEmulator.py
+++ neopixelEmulator.py
@@ -1,13 +1,5 @@
 #!/usr/bin/env python3
-
-#Import lines for dev build of pygame on Ubuntu (remove this pair of lines for raspberry pi)
-#I added them so I could develop in Thonny on Ubuntu using the latested development build of 
-#pygame, as the packaged version of pygame that Thonny offered did not detect the full set of
-#features of the game controllers I was using.
-import sys, math
-sys.path.append('/usr/local/lib/python3.5/dist-packages/pygame-1.9.4.dev0-py3.5-linux-i686.egg')
-
-import pygame
+import pygame, math, time
 
 def Color(red, green, blue, white = 0):
 	"""Convert the provided red, green, blue color to a 24-bit color value.
@@ -45,7 +37,7 @@ class Neopixel_Emulator(object):
         
         #Store parameters
         self.brightness = brightness
-        self.numLEDs = num
+        self._numLEDs = num
         
         #Set initial values
         self._initialised = False
@@ -76,7 +68,7 @@ class Neopixel_Emulator(object):
     
     def initialiseLEDCircle(self, num):
         """ Set LED positions in a circle """
-        LEDsize = int( 20 / num ) + 10
+        self._LEDsize = int( 20 / num ) + 10
         radius = int( num * 10 )
         
         #Position LEDs in a circle
@@ -84,23 +76,30 @@ class Neopixel_Emulator(object):
             angleRad = math.pi - (i * 2 * math.pi / num)
             x = int( radius * math.sin( angleRad ) + self._width / 2 )
             y = int( radius * math.cos( angleRad ) + self._height / 2 )
-            self._led_pos.append( [x,y] )
-            pygame.draw.circle(self.screen, (int(155 * i / num)+100,0,0), (x,y), LEDsize)
-
-        # Update the screen 
-        pygame.display.flip()
+            self._led_pos[i] = [x,y]
         
         
     def begin(self):
-        """Initialize library, must be called once before other functions are
-        called.
+        """ Initialize library, must be called once before other functions are
+            called.
         """
         self._initialised = True
         
 
     def show(self):
-        """Update the display with the data from the LED buffer."""
-        if _initialised :
+        """ Update the display with the data from the LED buffer. """
+        if self._initialised :
+            #Redraw all the neopixels
+            num = self.numPixels()
+            for i in range( 0, num ):
+                #Draw LED outline if completely off
+                colour = self._led_data[i]
+                if (colour[0] == 0) and (colour[1] == 0) and (colour[2] == 0):
+                    pygame.draw.circle(self.screen, ([16,16,16]), self._led_pos[i], self._LEDsize )
+                    pygame.draw.circle(self.screen, ([0,0,0]), self._led_pos[i], self._LEDsize - 2 )
+                else:
+                    pygame.draw.circle(self.screen, colour, self._led_pos[i], self._LEDsize)
+            
             # Update the screen 
             pygame.display.flip()
         else:
@@ -109,38 +108,39 @@ class Neopixel_Emulator(object):
         
         
     def setPixelColor(self, n, color):
-        """Set LED at position n to the provided 24-bit color value (in RGB order).
+        """ Set LED at position n to the provided 24-bit color value (in RGB order).
         """
         self._led_data[n] = color
 
 
     def setPixelColorRGB(self, n, red, green, blue, white = 0):
-        """Set LED at position n to the provided red, green, and blue color.
-        Each color component should be a value from 0 to 255 (where 0 is the
-        lowest intensity and 255 is the highest intensity).
+        """ Set LED at position n to the provided red, green, and blue color.
+            Each color component should be a value from 0 to 255 (where 0 is the
+            lowest intensity and 255 is the highest intensity).
         """
         self.setPixelColor(n, Color(red, green, blue, white))
 
 
     def setBrightness(self, brightness):
-        """Scale each LED in the buffer by the provided brightness.  A brightness
-        of 0 is the darkest and 255 is the brightest.
+        """ Scale each LED in the buffer by the provided brightness.  A brightness
+            of 0 is the darkest and 255 is the brightest.
         """
 
 
     def getPixels(self):
-        """Return an object which allows access to the LED display data as if 
-        it were a sequence of 24-bit RGB values.
+        """ Return an object which allows access to the LED display data as if 
+            it were a sequence of 24-bit RGB values.
         """
         return self._led_data
     
 
     def numPixels(self):
-        """Return the number of pixels in the display."""
-        return ws.ws2811_channel_t_count_get(self._channel)
+        """ Return the number of pixels in the display."""
+        return self._numLEDs
+
 
     def getPixelColor(self, n):
-        """Get the 24-bit RGB color value for the LED at position n."""
+        """ Get the 24-bit RGB color value for the LED at position n."""
         return self._led_data[n]
 
 
@@ -162,9 +162,52 @@ def main():
     
     # Intialize the library (must be called once before other functions).
     strip.begin()
-
+    
+    #In the emulator we need to check for the pygame QUIT event to exit our main program loop and shut down pygame.
+    #This will enable to emulator program to exit when the user closes the window.
     keepRunning = True
+    i = 0
+    c = 0
+    cmax = 2 * strip.numPixels()
+    mode = 0
+    
     while keepRunning == True :
+        #Code to animate neopixels goes here
+        
+        #Set colour
+        c1 = int( 255 * c / cmax )
+        c2 = 0 
+        c3 = 255 - c1
+        if mode == 0:
+            r = c3
+            b = c2
+            g = c1
+        elif mode == 1:
+            r = c2
+            b = c1
+            g = c3
+        elif mode == 2:
+            r = c1
+            b = c3
+            g = c2
+            
+        strip.setPixelColor(i,[r,g,b])
+        strip.show()
+        time.sleep(0.1)
+        
+        i = i + 1
+        if i >= strip.numPixels():
+            i = 0
+
+        c = c + 1
+        if c >= cmax:
+            c = 0
+            mode = mode + 1
+            
+        if mode > 2:
+            mode = 0
+            
+        #Check for quit event (when user closes pygame window)
         for event in pygame.event.get(): # User did something
             if event.type == pygame.QUIT: # If user clicked close
                 keepRunning = False # Flag that we are done so we exit this loop
